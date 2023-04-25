d229b83a9383423be14ce3ca3f2400f27917ad77
Saul Ceja <saulceja08@gmail.com>
2023-04-24 18:19:40 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+//Remove <main> element from the html code
+const removeMainElement = document.querySelector('main')
+removeMainElement.remove();
+
+//Add a new element <h1> to DOM (createElement and Append)
+var newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = `Saul is the champion`
+document.body.append(newHeader)
+
